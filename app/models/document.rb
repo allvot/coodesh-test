b4145b2cde4cbca396f0dc8abb413509f228b587
Alvaro Alday <alvaro.alday@baam.com>
2025-10-07 17:55:27 -0600

    Add attachments to document model

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,3 +1,14 @@
 class Document < ApplicationRecord
+  has_one_attached :file
+
+  # ASSOCIATIONS
+  # ------------
+
   belongs_to :vault
+
+  # VALIDATIONS
+  # ------------
+
+  validates :name, presence: true
+  validates :vault, presence: true
 end
--- spec/models/document_spec.rb
+++ spec/models/document_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe Document, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Document do
+  context "with valid attributes" do
+    subject(:document) { create(:document) }
+
+    it { is_expected.to be_valid }
+    it("has a name") { expect(document.name).to be_present }
+    it("has a vault") { expect(document.vault).to be_present }
+  end
+
+  context "with blank attributes" do
+    subject(:document) { Document.new }
+
+    before { document.valid? }
+
+    it { is_expected.to be_invalid }
+    it("has a name cant be blank error") { expect(document.errors[:name]).to include("can't be blank") }
+    it("has a vault must exist error") { expect(document.errors[:vault]).to include("must exist") }
+  end
 end
